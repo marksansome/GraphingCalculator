c25f6150f413f88f835391b715363f733d266d34
Colin Moffat <cmoffat@mail.uoguelph.ca>
2016-10-19 16:11:34 -0400

    Started work on basic calculator GUI

--- Modules/UI.py
+++ Modules/UI.py
@@ -11,15 +11,24 @@ def printtext():
     print string   
 
 root.title('Name')
-
-e = Entry(root)
-e.pack()
+frame = Frame(root)
+e = Entry(root, width = 40)
+e.grid()
 e.focus_set()
+num = [None]*10
+for i in range(0,10):
+	num[i] = Button(frame, text=i)
 
-b = Button(root,text='okay',command=printtext)
-d = Button(root, text='hello')
-e = Button(root, text='world')
-d.pack(side=LEFT)
-e.pack(side=LEFT)
-b.pack(side='bottom')
+frame.grid()
+
+num[1].grid(row=0, column = 0)
+num[2].grid(row=0, column = 1)
+num[3].grid(row=0, column = 2)
+num[4].grid(row=1, column = 0)
+num[5].grid(row=1, column = 1)
+num[6].grid(row=1, column = 2)
+num[7].grid(row=2, column = 0)
+num[8].grid(row=2, column = 1)
+num[9].grid(row=2, column = 2)
+num[0].grid(row=3, column = 1)
 root.mainloop()
