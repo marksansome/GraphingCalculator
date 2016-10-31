73840ea78fb12d78b049f764ae272bcdb55aa76e
Mumrau <mathias.chevalier@etu.unice>
2016-10-31 10:22:18 -0400

    Replacing value with the temporary table

--- Modules/Maths/Assets.py
+++ Modules/Maths/Assets.py
@@ -7,8 +7,17 @@ domainUpBound = 100
 step = 1
 
 #Replaces the variable named "variable" to the value "value" in the entire tree
-def replaceVariables(variable, value):
-	for i in Tree.table:
-		if Tree.getAtIndex(i) == variable:
-			Tree.setAtIndex(i, value)
+def replaceVariables(variable, value, table):
+	result = list(table)
+	for i in range(len(Tree.table)):
+		if table[i] == variable:
+			result[i] = value
+	return result
 
+#Iterates and create a table from the low boundary to the up boundary
+#TODO:maybe we need to check wheter the boundaries are valid or not
+def iteratesDomain():
+	resultTable = []
+	for i in range((domainUpBound - domainLowBound) / step):
+		#resultTable.append[]
+		i += 1
--- Tests/Maths/TestAssets.py
+++ Tests/Maths/TestAssets.py
@@ -1,10 +1,11 @@
 from Global import Tree
 from Maths import Assets
 
+#Testing replacments
 Tree.table = []
 for i in range(30):
 	Tree.table.append("x")
 
-Assets.replaceVariables("x", 2)
+print Assets.replaceVariables("x", 2, Tree.table)
 
-print Tree.table
+
